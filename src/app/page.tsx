6b345f7500673f197d4985a1aabe076fd921208b
DUAAPIRZADA22 <pirzadaduaa87@gmail.com>
2024-11-24 21:36:23 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,3 @@ const HomePage = () => {
 }
 
 export default HomePage;
-// total 5 cheezien thi ek home page tha wo to phele se hi ha or 1 menu tha to uska zahir ha koi page nai hoga 
-// isliye 3 banaye jab us 3 ko click krengy to ham respective page ma ponch jayengy
-// ab mujhe y bataien ke aap contact k page ma kia chah rhi ke kia show ho?
-
-// contact waala component ?
-// wo kahan banaya ha ?
-// bnaay e ni cuz meny jldi jldi m bss jo ai smjh tension m bnai ky date reh gai thi ek din bss me bna dun?
-// gi bana dega 
-// or component nai anyega
